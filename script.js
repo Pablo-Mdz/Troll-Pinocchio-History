3d8103ec6fcd0dc014cce07cc3e50cf62bee32e7
Pablo-Mdz <pablocigoy@gmail.com>
2023-11-28 13:55:31 +0100

    test

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
-async function sendScript(scriptText){
+async function sendScript(scriptText) {
     const lines = scriptText.split(/[\n\t]+/).map(line => line.trim()).filter(line => line);
     main = document.querySelector("#main"),
-    textarea = main.querySelector(`div[contenteditable="true"]`)
-    
-    if(!textarea) throw new Error("No conversation is open")
-    
-    for(const line of lines){
+        textarea = main.querySelector(`div[contenteditable="true"]`)
+    // tst
+    if (!textarea) throw new Error("No conversation is open")
+
+    for (const line of lines) {
         console.log(line)
-    
         textarea.focus();
         document.execCommand('insertText', false, line);
-        textarea.dispatchEvent(new Event('change', {bubbles: true}));
-    
+        textarea.dispatchEvent(new Event('change', { bubbles: true }));
+
         setTimeout(() => {
             (main.querySelector(`[data-testid="send"]`) || main.querySelector(`[data-icon="send"]`)).click();
         }, 100);
-        
-        if(lines.indexOf(line) !== lines.length - 1) await new Promise(resolve => setTimeout(resolve, 250));
+
+        if (lines.indexOf(line) !== lines.length - 1) await new Promise(resolve => setTimeout(resolve, 250));
     }
-    
+
     return lines.length;
 }
 
